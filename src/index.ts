b8669a1ddad0c8227cb649f4327d7a5a51f847ce
DrewsAbuse <andrii.bakanov@shelf.io>
2023-06-27 15:32:43 +0200

    feat: INFRA-3647 add documentClientConfig

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import {captureAWSv3Client} from 'aws-xray-sdk-core';
 import type {DynamoDBClientConfig} from '@aws-sdk/client-dynamodb';
 import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
+import type {TranslateConfig} from '@aws-sdk/lib-dynamodb';
 import {DynamoDBDocumentClient} from '@aws-sdk/lib-dynamodb';
 
-type GetClientParams = {credentials?: Credentials; clientConfig: DynamoDBClientConfig};
+type GetClientParams = {
+  credentials?: Credentials;
+  clientConfig: DynamoDBClientConfig;
+  documentClientConfig: TranslateConfig;
+};
 
 const isTest = process.env.JEST_WORKER_ID;
 const endpoint = process.env.DYNAMODB_ENDPOINT;
@@ -37,7 +42,10 @@ const getCredentials = (credentials?: Credentials) => {
 };
 
 export function getDocumentClient(params?: GetClientParams): DynamoDBClient {
-  const ddbDocumentClient = DynamoDBDocumentClient.from(getDDBClient(params));
+  const ddbDocumentClient = DynamoDBDocumentClient.from(
+    getDDBClient(params),
+    params?.documentClientConfig
+  );
 
   if (process.env.AWS_XRAY_DAEMON_ADDRESS) {
     captureAWSv3Client(ddbDocumentClient);
